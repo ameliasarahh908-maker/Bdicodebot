da587abbfd77a9c44c14c3e6fdcf6d34dcc83dc7
ameliasarahh908-maker <ameliasarahh908@gmail.com>
2026-07-14 02:23:53 +0700

    Update upload.py

--- states/upload.py
+++ states/upload.py
@@ -3,6 +3,7 @@ from aiogram.fsm.state import State, StatesGroup
 class UploadState(StatesGroup):
     waiting_media = State()
     confirm_done = State()
+    choose_share = State()
     choose_access = State()
     set_price = State()
     set_title = State()
