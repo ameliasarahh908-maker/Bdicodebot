d14e56f4032995e9966f20f81cf45a594e842ec4
ameliasarahh908-maker <ameliasarahh908@gmail.com>
2026-07-13 14:36:19 +0700

    Update config_vip.py

--- config_vip.py
+++ config_vip.py
@@ -8,49 +8,49 @@ VIP_PACKAGES = {
         "id": "vip1",
         "name": "💎 VIP 1 Hari",
         "days": 1,
-        "price": 15000,
+        "price": 20000,
     },
 
     "vip3": {
         "id": "vip3",
         "name": "💎 VIP 3 Hari",
         "days": 3,
-        "price": 25000,
+        "price": 40000,
     },
 
     "vip5": {
         "id": "vip5",
         "name": "💎 VIP 5 Hari",
         "days": 5,
-        "price": 35000,
+        "price": 60000,
     },
 
     "vip7": {
         "id": "vip7",
         "name": "💎 VIP 7 Hari",
         "days": 7,
-        "price": 45000,
+        "price": 80000,
     },
 
     "vip10": {
         "id": "vip10",
         "name": "💎 VIP 10 Hari",
         "days": 10,
-        "price": 50000,
+        "price": 100000,
     },
 
     "vip20": {
         "id": "vip20",
         "name": "💎 VIP 20 Hari",
         "days": 20,
-        "price": 70000,
+        "price": 150000,
     },
 
     "vip30": {
         "id": "vip30",
         "name": "💎 VIP 30 Hari",
         "days": 30,
-        "price": 100000,
+        "price": 200000,
     },
 
 }
