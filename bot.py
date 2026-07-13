0f6391284a3079123581e038363c3da3ff0185a7
ameliasarahh908-maker <ameliasarahh908@gmail.com>
2026-07-13 23:30:07 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -33,10 +33,15 @@ dp = Dispatcher(storage=storage)
 # =========================
 from handlers.start import router as start_router
 from handlers.check_sub import router as check_sub_router
-
+from handlers.vip import router as vip_router
+from handlers.search import router as search_router
+from handlers.top import router as top_router
 
 # =========================
 # REGISTER ROUTERS
 # =========================
 dp.include_router(start_router)
 dp.include_router(check_sub_router)
+dp.include_router(vip_router)
+dp.include_router(search_router)
+dp.include_router(top_router)
