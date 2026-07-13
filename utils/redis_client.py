0e0bd374414e49085f123b31150737e2272d6ca3
ameliasarahh908-maker <ameliasarahh908@gmail.com>
2026-07-13 22:17:32 +0700

    Update redis_client.py

--- utils/redis_client.py
+++ utils/redis_client.py
@@ -1,7 +1,14 @@
 import os
 from redis.asyncio import Redis
 
-redis = Redis.from_url(
-    os.getenv("REDIS_URL"),
-    decode_responses=True
-)
+REDIS_URL = os.getenv("REDIS_URL")
+
+redis_client = None
+
+if REDIS_URL:
+    redis_client = Redis.from_url(
+        REDIS_URL,
+        decode_responses=True
+    )
+else:
+    print("⚠️ REDIS_URL not set, Redis disabled")
