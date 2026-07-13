d1e0317f1f4dc175ce086c74c7a707da4c737c55
ameliasarahh908-maker <ameliasarahh908@gmail.com>
2026-07-13 14:33:36 +0700

    Update config.py

--- config.py
+++ config.py
@@ -12,34 +12,25 @@ TIMEZONE = "Asia/Jakarta"
 # BOT
 # =========================
 BOT_TOKEN = os.getenv("BOT_TOKEN")
-BOT_USERNAME = "DecoderFileBot"
-BOT_URL = f"https://t.me/{BOT_USERNAME}"
+BOT_USERNAME = "ZyxFidxBot"
+
 # =========================
 # DATABASE
 # =========================
 DATABASE_URL = os.getenv("DATABASE_URL")
 
 # =========================
-# PAYMENT
+# PAYMENT (BayarGG)
 # =========================
 BAYARGG_API_KEY = os.getenv("BAYARGG_API_KEY")
 BAYARGG_MERCHANT = os.getenv("BAYARGG_MERCHANT")
 BAYARGG_WEBHOOK_SECRET = os.getenv("BAYARGG_WEBHOOK_SECRET")
-# =========================
-# CHANNEL / GROUP
-# =========================
-CHANNEL_ID = int(os.getenv("CHANNEL_ID", "-1004395938795"))
-GROUP_ID = int(os.getenv("GROUP_ID", str(CHANNEL_ID)))
 
 # =========================
-# WITHDRAW
+# CHANNEL & GROUP
 # =========================
-WITHDRAW_CHANNEL_ID = int(
-    os.getenv(
-        "WITHDRAW_CHANNEL_ID",
-        str(CHANNEL_ID)
-    )
-)
+CHANNEL_ID = -1004419102427
+GROUP_ID = -1004437365690
 
 # =========================
 # ADMIN
@@ -49,14 +40,15 @@ ADMIN_IDS = [
     for x in os.getenv("ADMIN_IDS", "6847035364").split(",")
     if x.strip().isdigit()
 ]
+
 # =========================
 # VALIDATION
 # =========================
 if not BOT_TOKEN:
     raise ValueError("BOT_TOKEN belum di-set di .env")
 
-if not DATABASE_URL:
-    raise ValueError("DATABASE_URL belum di-set di .env")
-
 if not BAYARGG_API_KEY:
-    raise ValueError("BAYARGG_API_KEY belum di-set di .env / Railway Variables")
+    raise ValueError("BAYARGG_API_KEY belum di-set")
+
+if not BAYARGG_WEBHOOK_SECRET:
+    raise ValueError("BAYARGG_WEBHOOK_SECRET belum di-set")
