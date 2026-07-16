f3e8be6d738ca456008daece7b70795937c802d3
ameliasarahh908-maker <ameliasarahh908@gmail.com>
2026-07-16 22:21:15 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -53,7 +53,7 @@ from handlers.withdraw import (
 from handlers.admin import router as admin_router
 from handlers.notify import router as notify_router
 from handlers.open_menu import router as open_menu_router
-
+from handlers.test import router as test_router
 
 # =========================
 # REGISTER ROUTERS
@@ -84,7 +84,7 @@ dp.include_router(notify_router)
 
 # OPEN FILE MENU
 dp.include_router(open_menu_router)
-
+dp.include_router(test_router
 
 # =========================
 # MAIN (ENTRY POINT)
