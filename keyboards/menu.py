87f676fa11c1206bf8bcf2e648203e178306d263
ameliasarahh908-maker <ameliasarahh908@gmail.com>
2026-07-17 03:33:20 +0700

    Update menu.py

--- keyboards/menu.py
+++ keyboards/menu.py
@@ -1,4 +1,4 @@
-from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 from utils.user_lang import get_user_language
 
 
@@ -6,63 +6,28 @@ async def home_kb(user_id):
 
     lang = await get_user_language(user_id)
 
-    # =========================
-    # TEXT
-    # =========================
     if lang == "id":
-        t = {
-            "upload": "📤 Upload File",
-            "get": "📥 Ambil File",
-            "top": "🏆 File Teratas",
-            "new": "🆕 Kode Baru",
-            "search_code": "🔎 Cari Kode",
-            "search_price": "💰 Cari Harga",
-            "account": "👤 Akun",
-            "vip": "💎 VIP",
-            "help": "ℹ️ Bantuan"
-        }
+        store = "🏪 Store"
+        top = "🏆 Top 10 Code"
+        account = "👤 Akun"
+        upgrade = "💎 Upgrade"
     else:
-        t = {
-            "upload": "📤 Upload File",
-            "get": "📥 Get File",
-            "top": "🏆 Top File",
-            "new": "🆕 New Code",
-            "search_code": "🔎 Search Code",
-            "search_price": "💰 Search Price",
-            "account": "👤 Account",
-            "vip": "💎 VIP",
-            "help": "ℹ️ Help"
-        }
+        store = "🏪 Store"
+        top = "🏆 Top 10 Code"
+        account = "👤 Account"
+        upgrade = "💎 Upgrade"
 
-    # =========================
-    # KEYBOARD
-    # =========================
-    return InlineKeyboardMarkup(
-        inline_keyboard=[
 
+    return ReplyKeyboardMarkup(
+        keyboard=[
             [
-                InlineKeyboardButton(text=t["upload"], callback_data="upfile"),
-                InlineKeyboardButton(text=t["get"], callback_data="getfile")
+                KeyboardButton(text=store),
+                KeyboardButton(text=top)
             ],
-
             [
-                InlineKeyboardButton(text=t["top"], callback_data="top_file"),
-                InlineKeyboardButton(text=t["new"], callback_data="new_code")
-            ],
-
-            [
-                InlineKeyboardButton(text=t["search_code"], callback_data="search_code"),
-                InlineKeyboardButton(text=t["search_price"], callback_data="search_price")
-            ],
-
-            [
-                InlineKeyboardButton(text=t["account"], callback_data="account"),
-                InlineKeyboardButton(text=t["vip"], callback_data="vvip")
-            ],
-
-            [
-                InlineKeyboardButton(text=t["help"], callback_data="help")
+                KeyboardButton(text=account),
+                KeyboardButton(text=upgrade)
             ]
-
-        ]
+        ],
+        resize_keyboard=True
     )
