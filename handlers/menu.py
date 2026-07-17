434569d26176dd497943cad0b771e6fbdf325ae9
ameliasarahh908-maker <ameliasarahh908@gmail.com>
2026-07-18 00:01:21 +0700

    Update menu.py

--- handlers/menu.py
+++ handlers/menu.py
@@ -4,12 +4,9 @@ from aiogram.types import Message
 router = Router()
 
 
-@router.message(F.text == "🏪 Store")
-async def store(message: Message):
-    await message.answer("🏪 Store sedang disiapkan.")
-
-
 @router.message(F.text == "🏆 Top 10 Code")
 async def top(message: Message):
+
     from handlers.top import top_command
+
     await top_command(message)
