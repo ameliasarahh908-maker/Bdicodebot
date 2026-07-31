f35533cfa54eb2ad540747f5faf62f0e8373e778
ameliasarahh908-maker <ameliasarahh908@gmail.com>
2026-07-31 22:07:34 +0700

    Update auto_delete.py

--- tasks/auto_delete.py
+++ tasks/auto_delete.py
@@ -1,32 +1,59 @@
 import asyncio
-import time
 from database import get_pool
 
+
 async def auto_delete_worker():
+
+    print("🗑 Auto delete worker running...")
+
     while True:
+
         try:
+
             pool = await get_pool()
 
-            now = int(time.time())
-
-            # ambil file expired
             rows = await pool.fetch(
-                "SELECT code FROM files WHERE expires_at IS NOT NULL AND expires_at < $1",
-                now
+                """
+                SELECT 
+                    id,
+                    code
+                FROM codes
+                WHERE expires_at IS NOT NULL
+                AND expires_at < NOW()
+                """
             )
 
             for row in rows:
+
+                code_id = row["id"]
                 code = row["code"]
 
-                # delete dari DB
+                # hapus media terkait
                 await pool.execute(
-                    "DELETE FROM files WHERE code=$1",
-                    code
+                    """
+                    DELETE FROM medias
+                    WHERE code_id=$1
+                    """,
+                    code_id
                 )
 
-                print(f"🗑 Deleted expired file: {code}")
+                # hapus code
+                await pool.execute(
+                    """
+                    DELETE FROM codes
+                    WHERE id=$1
+                    """,
+                    code_id
+                )
+
+                print(
+                    f"🗑 Deleted expired code: {code}"
+                )
 
         except Exception as e:
-            print("AUTO DELETE ERROR:", e)
+            print(
+                "AUTO DELETE ERROR:",
+                e
+            )
 
-        await asyncio.sleep(60)  # cek tiap 1 menit
+        await asyncio.sleep(60)
